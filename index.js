48c580c98e5a907614725fc2d8ccc94980aa01d5
Jonathan Persson <persson.jonathan@gmail.com>
2017-12-18 16:38:39 +0100

    Add converter [WIP]

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-const glob = require('glob');
+#!/usr/bin/env node
+
 const fs = require('fs');
+const process = require('process');
+const path = require('path');
 const { exec } = require('child_process');
+const glob = require('glob');
+
+const convertJsonToTypes = require('./convert');
 
 const flatten = arr => arr.reduce((a, b) => a.concat(b), []);
 const unique = arr => arr.filter((elem, pos, arr2) => arr2.indexOf(elem) == pos);
 const diff = (arr1, arr2) => arr1.filter(i => arr2.indexOf(i) < 0);
 const uniqueFilesFromGlobPatterns = patterns => unique(flatten(patterns.map(pattern => glob.sync(pattern))));
 
-const convertJsonToTypes = json => {
-    // @todo implement
-    return json;
+const configArg = process.argv.find(x => x.startsWith('--config='));
+
+if (!configArg) {
+    return console.error('No configuration file for `csharp-models-to-typescript` provided.');
 }
 
+const configPath = configArg.substr('--config='.length);
 let config;
 
 try {
-    unparsedConfig = fs.readFileSync('./csharp-models-to-json.json', 'utf8');
+    unparsedConfig = fs.readFileSync(configPath, 'utf8');
+} catch (error) {
+    return console.error(`Configuration file "${configPath}" not found.`);
+}
+
+try {
     config = JSON.parse(unparsedConfig);
 } catch (error) {
-    return console.error(error);
+    return console.error(`Configuration file "${configPath}" contains invalid JSON.`);
 }
 
 const include = config.include || [];
@@ -27,18 +40,31 @@ const output = config.output || 'types.json';
 
 const files = diff(uniqueFilesFromGlobPatterns(include), uniqueFilesFromGlobPatterns(exclude));
 
-exec(`dotnet run --project lib/csharp-models-to-json --files=${files.join(',')}`, (err, stdout) => {
+const dotnetProject = path.join(__dirname, 'lib/csharp-models-to-json');
+
+let timer = process.hrtime();
+
+exec(`dotnet run --project ${dotnetProject} --files=${files.join(',')}`, (err, stdout) => {
     if (err) {
         return console.error(err);
     }
 
-    const types = convertJsonToTypes(stdout);
+    let json;
+
+    try {
+        json = JSON.parse(stdout);
+    } catch (error) {
+        return console.error('The output from `csharp-models-to-json` contains invalid JSON.');
+    }
+
+    const types = convertJsonToTypes(json);
 
     fs.writeFile(output, types, err => {
         if (err) {
             return console.error(err);
         }
-    
-        console.log('Done!');
-    }); 
+
+        timer = process.hrtime(timer);
+        console.log('Done in %d.%d seconds.', timer[0], timer[1]);
+    });
 });
