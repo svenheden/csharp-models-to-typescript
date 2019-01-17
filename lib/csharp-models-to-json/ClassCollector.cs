4c9f8a3d054e43e7fbae9063aefe60df5cff7381
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-01-17 14:26:20 +0100

    Add ReadOnlyList to arguments and refactor slightly

--- lib/csharp-models-to-json/ClassCollector.cs
+++ lib/csharp-models-to-json/ClassCollector.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -27,28 +28,40 @@ namespace CSharpModelsToJson
 
     public class ClassCollector : CSharpSyntaxWalker
     {
-        public readonly List<Class> Items = new List<Class>();
+        public readonly List<Class> Classes = new List<Class>();
 
         public override void VisitClassDeclaration(ClassDeclarationSyntax node)
         {
-            var item = new Class() {
+            var syntaxClass = new Class()
+            {
                 ClassName = node.Identifier.ToString(),
                 Fields = node.Members.OfType<FieldDeclarationSyntax>()
-                    .Where(field => !field.Modifiers.Any(modifier => modifier.ToString() == "const" || modifier.ToString() == "static" || modifier.ToString() == "private"))
-                    .Select(field => new Field {
-                        Identifier = field.Declaration.Variables.First().GetText().ToString(),
-                        Type = field.Declaration.Type.ToString(),
-                    }),
+                    .Where(field => IsAccessible(field.Modifiers))
+                    .Select(GetField),
                 Properties = node.Members.OfType<PropertyDeclarationSyntax>()
-                    .Where(property => !property.Modifiers.Any(modifier => modifier.ToString() == "const" || modifier.ToString() == "static" || modifier.ToString() == "private"))
-                    .Select(property => new Property {
-                        Identifier = property.Identifier.ToString(),
-                        Type = property.Type.ToString(),
-                    }),
+                    .Where(property => IsAccessible(property.Modifiers))
+                    .Select(GetProperty),
                 BaseClasses = node.BaseList?.Types.ToString(),
             };
 
-            this.Items.Add(item);
+            Classes.Add(syntaxClass);
         }
+
+        private static bool IsAccessible(SyntaxTokenList modifiers)
+        {
+            return modifiers.Any(modifier => modifier.ToString() == "const" || modifier.ToString() == "static" || modifier.ToString() == "private");
+        }
+
+        private static Field GetField(FieldDeclarationSyntax field) => new Field
+        {
+            Identifier = field.Declaration.Variables.First().GetText().ToString(), 
+            Type = field.Declaration.Type.ToString(),
+        };
+
+        private static Property GetProperty(PropertyDeclarationSyntax property) => new Property
+        {
+            Identifier = property.Identifier.ToString(), 
+            Type = property.Type.ToString(),
+        };
     }
 }
