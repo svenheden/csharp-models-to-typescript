62a1ba1e60df0b70fa2b5e62d48d2cb586fc9dda
Rodrigo Cesar <rodrigo@fullgauge.com.br>
2024-06-19 15:17:19 -0300

    includeComments defaults to True

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const output = config.output || 'types.d.ts';
 const converter = createConverter({
     customTypeTranslations: config.customTypeTranslations || {},
     namespace: config.namespace,
-    includeComments: config.includeComments || false,
+    includeComments: config.includeComments ?? true,
     camelCase: config.camelCase || false,
     camelCaseOptions: config.camelCaseOptions || {},
     camelCaseEnums: config.camelCaseEnums || false,
